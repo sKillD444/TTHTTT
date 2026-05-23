3ce05a55baf9b3bafa0d0d3dc3bf1721385b28dd
Dang Ne <kdxk016@gmail.com>
2026-05-23 10:33:23 +0700

    Trien Khai HTTT

---
+++ __openerp__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Equipment Management",
+    "version": "2.0",
+    "author": "VoChungKhanhDang",
+    "description": "Module quản lý thiết bị: Sản phẩm, Loại, Thương hiệu và Kho",
+    "depends": ["base"],
+    "update_xml": [
+        "equipment_view.xml",
+        "equipment_demo.xml",
+        "equipment_menu.xml",
+    ],
+    "installable": True,
+}
---
+++ equipment.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from openerp.osv import fields, osv
+
+class equipment_category(osv.osv):
+    _name = 'equipment.category'
+    _columns = {
+        'name': fields.char('Tên loại', size=64, required=True),
+        'description': fields.text('Mô tả'),
+    }
+
+class equipment_brand(osv.osv):
+    _name = 'equipment.brand'
+    _columns = {
+        'name': fields.char('Tên thương hiệu', size=64, required=True),
+        'country': fields.char('Quốc gia'),
+    }
+
+class equipment_product(osv.osv):
+    _name = 'equipment.product'
+    _columns = {
+        'name': fields.char('Tên thiết bị', size=128, required=True),
+        'category_id': fields.many2one('equipment.category', 'Loại sản phẩm', required=True),
+        'brand_id': fields.many2one('equipment.brand', 'Thương hiệu'),
+        'qty_available': fields.float('Số lượng tồn', readonly=True),
+        'price': fields.float('Đơn giá'),
+        'specifications': fields.text('Thông số kỹ thuật'),
+    }
+
+class equipment_inventory(osv.osv):
+    _name = 'equipment.inventory'
+    _columns = {
+        'product_id': fields.many2one('equipment.product', 'Thiết bị', required=True),
+        'type': fields.selection([('in', 'Nhập kho'), ('out', 'Xuất kho')], 'Loại', required=True),
+        'quantity': fields.float('Số lượng', required=True),
+        'date': fields.datetime('Ngày thực hiện'),
+        'note': fields.char('Ghi chú'),
+    }
+    _defaults = {'date': lambda *a: fields.datetime.now()}
+
+    def create(self, cr, uid, vals, context=None):
+        res_id = super(equipment_inventory, self).create(cr, uid, vals, context=context)
+        inv = self.browse(cr, uid, res_id, context=context)
+        prod_obj = self.pool.get('equipment.product')
+        change = inv.quantity if inv.type == 'in' else -inv.quantity
+        new_qty = inv.product_id.qty_available + change
+        prod_obj.write(cr, uid, [inv.product_id.id], {'qty_available': new_qty})
+        return res_id
